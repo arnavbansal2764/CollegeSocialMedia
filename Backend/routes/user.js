b8bd95a9f35ad5af9c65b568fe4cdacb0e7316a5
Arnav Bansal <70932975+arnavbansal2764@users.noreply.github.com>
2024-01-19 23:52:50 +0530

    Update user.js

--- Backend/routes/user.js
+++ Backend/routes/user.js
@@ -1,8 +1,11 @@
 const { Router } = require("express");
 const router = Router();
-const {User}=require("../db/index");
+const { User } = require("../db/index");
+// const {default: mongoose} = require("mongoose");
+const { JWT_SECRET } = require("../config");
+const jwt = require("jsonwebtoken");
 
-router.post('/signup', async(req, res) => {
+router.post('/signup', async (req, res) => {
     // signup
     const username = req.body.username;
     const password = req.body.password;
@@ -13,44 +16,60 @@ router.post('/signup', async(req, res) => {
     const college = req.body.college;
     const name = req.body.name;
 
-    
-    const existUsername = await User.findOne({username});
-    const existSid = await User.findOne({sid});
-    const existEmail = await User.findOne({email});
-    if(existUsername)
-    {
+
+    const existUsername = await User.findOne({ username });
+    const existSid = await User.findOne({ sid });
+    const existemail = await User.findOne({ email });
+    if (existUsername) {
         res.status(403).json({
-            message:"Username already taken !!"
+            message: "Username already taken !!"
         })
     }
-    else if(existSid)
-    {
+    else if (existSid) {
         res.status(403).json({
-            message:"Student already registered"
+            message: "Student already registered"
         })
     }
-    else if(existEmail)
-    {
+    else if (existemail) {
         res.status(403).json({
-            message : "Email already registered"
+            message: "Email already registered"
         })
     }
-    else{
-    await User.create({
-        username,
-        name,
-        email,
-        password,
-        graduation,
-        sid,
-        branch,
-        college
-    })
-    res.json({
-        message: "User created successfully"
-    })
+    else {
+        await User.create({
+            username,
+            name,
+            email,
+            password,
+            graduation,
+            sid,
+            branch,
+            college
+        })
+        res.json({
+            message: "User created successfully"
+        })
     }
 
 });
 
+router.post('/signin', async (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const user = await User.find({
+        username,
+        password
+    })
+    if (user) {
+        const token = jwt.sign({ username }, JWT_SECRET);
+        res.json({ token })
+    }
+    else {
+        res.status(411).json({
+            message: "Incorrect email or password "
+        })
+    }
+
+});
 module.exports = router
