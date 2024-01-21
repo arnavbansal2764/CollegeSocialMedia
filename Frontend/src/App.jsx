508323e7096cb3588344428b1f3bb3214b489b65
Dushyant Bhardwaj <dushyantbha012@gmail.com>
2024-01-21 09:17:07 +0530

    chat front end

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,18 +2,13 @@ import { useState } from "react";
 import LoginPage from "./Public/loginPage/login";
 import RegisterPage from "./Public/registerPage/register";
 import Chat from "../src/components/chatfrontend/Chat"
-import io from "socket.io-client";
-
-const socket1 = io.connect("http://localhost:3001");
-const socket2 = io.connect("http://localhost:3001");
 
 function App() {
   return (
     <div>
       <RegisterPage />
       <LoginPage />
-      <Chat socket ={socket1} username="dushyant" room = "personal"/>
-      <Chat socket ={socket2} username="arnav" room = "personal"/>
+      <Chat username="dushyant" room = "personal"/>
     </div>
   );
 }
