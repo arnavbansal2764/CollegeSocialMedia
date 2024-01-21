4cfd72a451fb00b0308b12229e263564b50fbaee
Dushyantbha012 <89176588+Dushyantbha012@users.noreply.github.com>
2024-01-21 12:18:45 +0530

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,16 +1,28 @@
 import { useState } from "react";
 import LoginForm from "./components/LoginForm";
 import SignupForm from "./components/SignupForm";
-import Chat from "../src/components/chatfrontend/Chat"
+import Chat from "../src/components/chatfrontend/Chat";
 
 function App() {
-  const [globalUserName,setGlobalUserName]= useState("")
-  console.log("usernmae is", globalUserName)
+  const [globalUserName, setGlobalUserName] = useState("");
+  const [globalCollege, setGlobalCollege] = useState("");
+  console.log("usernmae is", globalUserName);
   return (
     <div>
-      <SignupForm globalUserName={globalUserName} setGlobalUserName={setGlobalUserName}/>
-      <LoginForm globalUserName={globalUserName} setGlobalUserName={setGlobalUserName}/>
-      <Chat username="dushyant" room = "personal"/>
+      <SignupForm
+        globalUserName={globalUserName}
+        setGlobalUserName={setGlobalUserName}
+        globalCollege={globalCollege}
+        setGlobalCollege={setGlobalCollege}
+      />
+      <LoginForm
+        globalUserName={globalUserName}
+        setGlobalUserName={setGlobalUserName}
+        globalCollege={globalCollege}
+        setGlobalCollege={setGlobalCollege}
+      />
+      <Chat username={globalUserName} room="personal" />
+      <Chat username="dushy" room="personal" /> 
     </div>
   );
 }
